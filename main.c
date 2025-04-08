232a3f6652a5e4ef9ec73288f693754fc1832d87
vasilikiyokaris@gmail.com <vasilikiyokaris@gamil.com>
2025-04-08 22:33:50 +0200

    Dice rolls working

--- main.c
+++ main.c
@@ -5,9 +5,18 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
+    int dice_rolls[100];
 
-   
-    // Your code goes here
+    for (int i = 0; i < 100; i++){
+        dice_rolls[i] = (rand()%6)+1;
+        // check for rolls 
+        // printf("roll %d\n" , dice_rolls[i]); 
+    }
+    
+    // Your code is here
+
+
+
 
     return 0;
 }
